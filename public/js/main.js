ce18cd8d54d5ce2398f674115b24edcd7698a996
twsouza <taynan.tws@gmail.com>
2017-04-07 22:36:31 -0300

    Adicionando cronometro e travando textarea

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,15 @@ campo.on("input",function(){
   $(".contador-palavras").text(conteudo.split(/\S+/).length-1 + " palavras");
   $(".contador-caracteres").text(conteudo.length + " caracteres");
 });
+
+campo.one("focus",function(){ // função 'one' serve para uma chamada apenas
+  var tempoRestante = $(".tempo-digitacao").text(); // pega o tempo
+  var cronometro = setInterval(function(){ // armazena o id do setInterval
+    tempoRestante--;
+    $(".tempo-digitacao").text(tempoRestante); //alterar o tempo na pagina
+    if(tempoRestante <= 0){        // se for menor q zero,
+      campo.attr("disabled", true);// desabilita o textarea
+      clearInterval(cronometro);   // e fecha o setInterval
+    }
+  }, 1000) // vai fazer toda a função dentro do setInterval dentro desse tempo
+})
