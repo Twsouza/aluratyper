345d0a76fcf1bf264340cb96d0d06f06b356359d
twsouza <taynan.tws@gmail.com>
2017-04-08 14:34:00 -0300

    Separando codigo em funções

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,45 @@
 var tempoInicial = $(".tempo-digitacao").text(); // guardando o tempo inicial
-
-var frase = $(".frase").text();
-var numeroPalavras = frase.split(" ").length;
-var tamanhoFrase = $(".tamanho-frase");
-tamanhoFrase.text(numeroPalavras + " palavras");
-
 var campo = $(".campo-digitacao");
-campo.on("input",function(){
-  var conteudo = campo.val();
-  $(".contador-palavras").text(conteudo.split(/\S+/).length-1 + " palavras");
-  $(".contador-caracteres").text(conteudo.length + " caracteres");
-});
 
-campo.one("focus",function(){ // função 'one' serve para uma chamada apenas
-  var tempoRestante = $(".tempo-digitacao").text(); // pega o tempo
-  var cronometro = setInterval(function(){ // armazena o id do setInterval
-    tempoRestante--;
-    $(".tempo-digitacao").text(tempoRestante); //alterar o tempo na pagina
-    if(tempoRestante <= 0){        // se for menor q zero,
-      campo.attr("disabled", true);// desabilita o textarea
-      clearInterval(cronometro);   // e fecha o setInterval
-    }
-  }, 1000) // vai fazer toda a função dentro do setInterval dentro desse tempo
+$(document).ready(function(){
+  atualizaFrase();
+  atualizaContadores();
+  inicializaContadores();
+  $("#botao-reiniciar").click(reiniciaJogo);
 })
 
-$("#botao-reiniciar").click(function(){
-  campo.val("");
-  campo.attr("disabled", false);
-  $(".contador-palavras").text("0 palavras");
-  $(".contador-caracteres").text("0 caracteres");
-  $(".tempo-digitacao").text(tempoInicial);
-})
+function atualizaFrase() {
+  var frase = $(".frase").text();
+  var numeroPalavras = frase.split(" ").length;
+  var tamanhoFrase = $(".tamanho-frase");
+  tamanhoFrase.text(numeroPalavras + " palavras");
+}
+
+function atualizaContadores() {
+  campo.on("input",function(){
+    var conteudo = campo.val();
+    $(".contador-palavras").text(conteudo.split(/\S+/).length-1 + " palavras"); //conta a quantidade de palavras, separando por um ou mais espaços(regex /\S+/), -1 serve para zerar o contador
+    $(".contador-caracteres").text(conteudo.length + " caracteres");//conta a quantidade de caracteres
+  });
+}
+
+function inicializaContadores() {
+  campo.one("focus",function(){ // função 'one' serve para uma chamada apenas
+    var tempoRestante = $(".tempo-digitacao").text(); // pega o tempo
+    var cronometro = setInterval(function(){ // armazena o id do setInterval
+      tempoRestante--;
+      $(".tempo-digitacao").text(tempoRestante); //alterar o tempo na pagina
+      if(tempoRestante <= 0){        // se for menor q zero,
+        campo.attr("disabled", true);// desabilita o textarea
+        clearInterval(cronometro);   // e fecha o setInterval
+      }
+    }, 1000) // vai fazer toda a função dentro do setInterval dentro desse tempo
+  })
+}
+function reiniciaJogo() {
+  campo.val("");//zera campo
+  campo.attr("disabled", false); //habilita textarea
+  $(".contador-palavras").text("0 palavras"); // zera contador-palavras
+  $(".contador-caracteres").text("0 caracteres"); // zera contador-caracteres
+  $(".tempo-digitacao").text(tempoInicial); //reinicia tempo
+}
