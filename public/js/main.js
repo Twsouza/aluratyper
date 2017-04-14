fa2c2b7a160c7cefaa4c257f11b9b6d9247fa64a
twsouza <taynan.tws@gmail.com>
2017-04-13 21:56:55 -0300

    Adicionando remover placar

--- public/js/main.js
+++ public/js/main.js
@@ -67,11 +67,32 @@ function inserePlacar(){
   var usuario = "Taynan";
   var numPalavras = $(".contador-palavras").text();
 
-  var linha = "<tr>"+
-                "<td>"+ usuario + "</td>"+
-                "<td>"+ numPalavras + "</td>"+
-              "</tr>";
+  var linha = novaLinha(usuario,numPalavras);
+  linha.find(".botao-remover").click(removeLinha);
+
   corpoTabela.prepend(linha);
+}
+
+function novaLinha(usuario,palavras){
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+    var link = $("<a>").attr("href","#").addClass("botao-remover");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    colunaRemover.append(link.append(icone));
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(event){
+    event.preventDefault();
+    $(this).parent().parent().remove();
 }
 
 function reiniciaJogo() {
